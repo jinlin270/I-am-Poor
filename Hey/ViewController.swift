c3bd3b867c8ee63211633bfcce747360f58e799e
jinlin270 <56476240+jinlin270@users.noreply.github.com>
2022-04-23 19:19:34 -0400

    Initial Commit

---
+++ Hey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hey
+//
+//  Created by Lin Jin on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
